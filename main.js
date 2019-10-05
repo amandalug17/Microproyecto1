5b26e89377b43c982b748dca1501f09dc4301c01
Amanda Gonzalez <amandalug17@gmail.com>
2019-10-05 01:14:01 -0400

    latest

---
+++ main.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+    var chart = new CanvasJS.Chart("myChart", {
+        animationEnabled: true,
+        title: {
+            text: ""
+        },
+        data: [{
+            type: "pie",
+            startAngle: 240,
+            yValueFormatString: "##0.00\"%\"",
+            indexLabel: "{label} {y}",
+            dataPoints: [
+                {y: 80, label: "Java"},
+                {y: 9, label: "HTML"},
+                {y: 9, label: "CSS"},
+                {y: 2, label: "JavaScript"}
+            ]
+        }]
+    });
+    chart.render();
+    
+    }
